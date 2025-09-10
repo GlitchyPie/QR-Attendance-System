2b48486b3235ac693b63e2f6e519cdbd877eeaa2
GlitchyPie <71139548+GlitchyPie@users.noreply.github.com>
2025-09-10 15:02:36 +0100

    Whoops, forgot to update function reference

--- FacultyView/urls.py
+++ FacultyView/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path("", views.faculty_view, name="faculty_view"),
     path("add_manually", views.add_manually, name="add_manually"),
     path("add_manually/<str:branch>/<str:section>/<int:year>/",views.add_manually_filtered, name="add_manually_filtered"),
-    path("add_manually/<str:branch>/<str:section>/",views.add_manually_filtered, name="add_manually_filtered_2"),
-    path("add_manually/<str:branch>/",views.add_manually_filtered, name="add_manually_filtered_3")
+    path("add_manually/<str:branch>/<str:section>/",views.add_manually_filtered_2, name="add_manually_filtered_2"),
+    path("add_manually/<str:branch>/",views.add_manually_filtered_3, name="add_manually_filtered_3")
 ]
