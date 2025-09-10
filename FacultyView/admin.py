66f0c96029d25df76c54cf4a9ef459b0297a6e7b
GlitchyPie <71139548+GlitchyPie@users.noreply.github.com>
2025-09-10 15:28:58 +0100

    Update admin.py with new models

--- FacultyView/admin.py
+++ FacultyView/admin.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Student, Section, Year, Branch
+from .models import Student, Section, Year, Branch, ClassName, Attendance
 
 admin.site.register(Student)
 admin.site.register(Section)
 admin.site.register(Year)
 admin.site.register(Branch)
+admin.site.register(ClassName)
+admin.site.register(Attendance)
