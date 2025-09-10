c834d261ac784fc3d89781345f41a1c8e8830a02
GlitchyPie <71139548+GlitchyPie@users.noreply.github.com>
2025-09-10 16:00:44 +0100

    Probably broken, but lets find out how badly

--- FacultyView/urls.py
+++ FacultyView/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.faculty_view, name="faculty_view"),
+    path("class/<str:className>/", views.faculty_view_class, name="faculty_view_class"),
     path("add_manually", views.add_manually, name="add_manually"),
     path("add_manually/<str:branch>/<str:section>/<int:year>/",views.add_manually_filtered, name="add_manually_filtered"),
     path("add_manually/<str:branch>/<str:section>/",views.add_manually_filtered_2, name="add_manually_filtered_2"),
--- FacultyView/views.py
+++ FacultyView/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from .models import Student
+from .models import Student, ClassName, Attendance
 import qrcode
 import socket
 from StudentView.views import present
 
 
-def qrgenerator(request):
+def qrgenerator(request,className = ""):
     #s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     #s.connect(("8.8.8.8", 80))
     #ip = s.getsockname()[0]
 
-    link = f"{request.scheme}://{request.META['HTTP_HOST']}:{request.META['SERVER_PORT']}/add_manually"
+    link = f"{request.scheme}://{request.META['HTTP_HOST']}:{request.META['SERVER_PORT']}/class/{className}/add_manually"
 
     def generate_qr_code(link):
         qr = qrcode.QRCode(
@@ -24,27 +24,38 @@ def qrgenerator(request):
         qr.add_data(link)
         qr.make(fit=True)
         img = qr.make_image(fill_color="black", back_color="white")
-        img.save("FacultyView/static/FacultyView/qrcode.png")
+        img.save("FacultyView/static/FacultyView/qrcode_{className}.png")
 
     generate_qr_code(link)
 
-def faculty_view(request):
+def faculty_view_class(request,className):
     if request.method == "POST":
         student_roll = request.POST["student_id"]
         student = Student.objects.get(s_roll=student_roll)
         if student in present:
             present.remove(student)
         return HttpResponseRedirect("/")
-
     else:
         qrgenerator(request)
         return render(
             request,
-            "FacultyView/FacultyViewIndex.html",
+            "FacultyView/FacultyViewClass.html",
             {
                 "students": present,
+                "className": className
             },
         )
+
+def faculty_view(request):
+    classes = ClassName.objects
+    return render(
+        request,
+        "FacultyView/FacultyViewIndex.html",
+        {
+            "classes",classes
+        },
+    )
+        
 
 def render_student_list(request,students):
     return render(
