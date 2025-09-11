6fa3ae18d362e8167f25d4db8eae35440f4702f0
GlitchyPie <71139548+GlitchyPie@users.noreply.github.com>
2025-09-11 12:21:35 +0100

    put int matching first

--- StudentView/urls.py
+++ StudentView/urls.py
@@ -2,9 +2,9 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
+    path("class/<int:classId>/student_entry",views.student_entry_id,name="student_entry_id"),
+    path("class/<int:classId>/submit_attendance",views.submit_attendance_id,name="submit_attendance_id"),      
     path("class/<str:className>/student_entry",views.student_entry_name,name="student_entry_name"),
     path("class/<str:className>/submit_attendance",views.submit_attendance_name,name="submit_attendance_name"),
-    path("class/<int:classId>/student_entry",views.student_entry_id,name="student_entry_id"),
-    path("class/<int:classId>/submit_attendance",views.submit_attendance_id,name="submit_attendance_id"),  
     path("submitted", views.submitted, name="submitted"),
 ]
