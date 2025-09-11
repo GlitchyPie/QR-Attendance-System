125e2927bb8f96114ff81a6e4d4fb8cdd8684aef
GlitchyPie <71139548+GlitchyPie@users.noreply.github.com>
2025-09-11 10:36:19 +0100

    tweaks

--- FacultyView/urls.py
+++ FacultyView/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 urlpatterns = [
     path("", views.faculty_view, name="faculty_view"),
     path("class/<str:className>/", views.faculty_view_class, name="faculty_view_class"),
-    path("add_manually/class/<str:className>", views.add_manually_class, name="add_manually_class"),
+    #path("add_manually/class/<str:className>", views.add_manually_class, name="add_manually_class"),
     #path("add_manually/<str:branch>/<str:section>/<int:year>/",views.add_manually_filtered, name="add_manually_filtered"),
     #path("add_manually/<str:branch>/<str:section>/",views.add_manually_filtered_2, name="add_manually_filtered_2"),
     #path("add_manually/<str:branch>/",views.add_manually_filtered_3, name="add_manually_filtered_3"),
--- FacultyView/views.py
+++ FacultyView/views.py
@@ -8,11 +8,7 @@ from StudentView.views import present
 import urllib.parse
 
 def qrgenerator(request,className = ""):
-    #s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    #s.connect(("8.8.8.8", 80))
-    #ip = s.getsockname()[0]
-
-    link = f"{request.scheme}://{request.META['HTTP_HOST']}:{request.META['SERVER_PORT']}/class/{urllib.parse.quote(className)}/add_manually"
+    link = f"{request.scheme}://{request.META['HTTP_HOST']}:{request.META['SERVER_PORT']}/class/{urllib.parse.quote(className)}/student_entry"
 
     def generate_qr_code(link,className):
         qr = qrcode.QRCode(
@@ -29,22 +25,16 @@ def qrgenerator(request,className = ""):
     generate_qr_code(link,className)
 
 def faculty_view_class(request,className):
-    if request.method == "POST":
-        student_roll = request.POST["student_id"]
-        student = Student.objects.get(s_roll=student_roll)
-        if student in present:
-            present.remove(student)
-        return HttpResponseRedirect("/")
-    else:
-        qrgenerator(request,className)
-        return render(
-            request,
-            "FacultyView/FacultyViewClass.html",
-            {
-                "students": present,
-                "className": className
-            },
-        )
+    qrgenerator(request,className)
+    present = []
+    return render(
+        request,
+        "FacultyView/FacultyViewClass.html",
+        {
+            "students": present,
+            "className": className
+        },
+    )
 
 def faculty_view(request):
     classes = ClassName.objects.all()
@@ -57,36 +47,36 @@ def faculty_view(request):
     )
         
 
-def render_student_list(request,students):
-    return render(
-        request,
-        "StudentView/StudentViewIndex.html",
-        {
-            "students": students,
-        },
-    )
+#def render_student_list(request,students):
+#    return render(
+#        request,
+#        "StudentView/StudentViewIndex.html",
+#        {
+#            "students": students,
+#        },
+#    )
 
-def add_manually(request):
-    students = Student.objects.all().order_by("s_roll")
-    return render_student_list(request, students)
+#def add_manually(request):
+#    students = Student.objects.all().order_by("s_roll")
+#    return render_student_list(request, students)
 
-def add_manually_class(request,className):
-    students = Student.objects.all().order_by("s_roll")
-    return render_student_list(request, students)
+#def add_manually_class(request,className):
+#    students = Student.objects.all().order_by("s_roll")
+#    return render_student_list(request, students)
 
-def add_manually_year(request, year):
-    students = Student.objects.filter(s_year=year)
-    return render_student_list(request, students)
+#def add_manually_year(request, year):
+#    students = Student.objects.filter(s_year=year)
+#    return render_student_list(request, students)
 
 #https://stackoverflow.com/questions/57989320/django-filter-foreignkey-related-model-filtering
-def add_manually_filtered(request, branch, section, year):
-    students = Student.objects.filter(s_branch__branch__iexact=branch).filter(s_section__section__iexact=section).filter(s_year__year=year)
-    return render_student_list(request, students)
+#def add_manually_filtered(request, branch, section, year):
+#    students = Student.objects.filter(s_branch__branch__iexact=branch).filter(s_section__section__iexact=section).filter(s_year__year=year)
+#    return render_student_list(request, students)
 
-def add_manually_filtered_2(request, branch, section):
-    students = Student.objects.filter(s_branch__branch__iexact=branch).filter(s_section__section__iexact=section)
-    return render_student_list(request, students)
+#def add_manually_filtered_2(request, branch, section):
+#    students = Student.objects.filter(s_branch__branch__iexact=branch).filter(s_section__section__iexact=section)
+#    return render_student_list(request, students)
 
-def add_manually_filtered_3(request, branch):
-    students = Student.objects.filter(s_branch__branch__iexact=branch)
-    return render_student_list(request, students)
+#def add_manually_filtered_3(request, branch):
+#    students = Student.objects.filter(s_branch__branch__iexact=branch)
+#    return render_student_list(request, students)
