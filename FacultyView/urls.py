48fd97e28e7d60f378d04302b15395c1050282e4
GlitchyPie <71139548+GlitchyPie@users.noreply.github.com>
2025-09-11 11:37:12 +0100

    typo

--- FacultyView/urls.py
+++ FacultyView/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 urlpatterns = [
     path("", views.faculty_view, name="faculty_view"),
     path("class/<str:className>/", views.faculty_view_class_name, name="faculty_view_class_name"),
-    path("class/<int:classId>/", views.faculty_view_class_ide, name="faculty_view_class_id"),
+    path("class/<int:classId>/", views.faculty_view_class_id, name="faculty_view_class_id"),
     #path("add_manually/class/<str:className>", views.add_manually_class, name="add_manually_class"),
     #path("add_manually/<str:branch>/<str:section>/<int:year>/",views.add_manually_filtered, name="add_manually_filtered"),
     #path("add_manually/<str:branch>/<str:section>/",views.add_manually_filtered_2, name="add_manually_filtered_2"),
