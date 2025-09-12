272f8030fab13fdde84ef0aac39ec65129a6c84c
GlitchyPie <71139548+GlitchyPie@users.noreply.github.com>
2025-09-12 10:30:16 +0100

    Update requirements and models

--- FacultyView/models.py
+++ FacultyView/models.py
@@ -1,8 +1,18 @@
 from datetime import datetime
 from django.db import models
+from django_case_insensitive_field import CaseInsensitiveFieldMixin
+
+class LowerCharField(CaseInsensitiveFieldMixin, models.CharField):
+    """[summary]
+    Makes django CharField case insensitive \n
+    Extends both the `CaseInsensitiveFieldMixin` and  CharField \n
+    Then you can import 
+    """
+    def __init__(self, *args, **kwargs):
+        super(CaseInsensitiveFieldMixin, self).__init__(*args, **kwargs)
 
 class Student(models.Model):
-    s_eml = models.CharField(max_length=100, primary_key=True)
+    s_eml = LowerCharField(max_length=100, primary_key=True)
     s_fname = models.CharField(max_length=30)
     s_lname = models.CharField(max_length=30)
 
@@ -17,7 +27,7 @@ class ClassName(models.Model):
 
 class Attendance(models.Model):
     s_class = models.ForeignKey(ClassName, on_delete=models.RESTRICT)
-    dte_date = models.DateField()
+    dte_date = models.DateTimeField()
     student = models.ForeignKey(Student, on_delete=models.RESTRICT)
 
     def __str__(self) -> str:
