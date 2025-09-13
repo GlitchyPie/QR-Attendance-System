e392d9b0cc908976c54a568fd0a7da8298c59853
GlitchyPie <71139548+GlitchyPie@users.noreply.github.com>
2025-09-13 20:54:56 +0100

    Make Faculty views use template inheritance

--- FacultyView/views.py
+++ FacultyView/views.py
@@ -67,38 +67,3 @@ def faculty_view(request):
             "classes": classes
         },
     )
-        
-
-#def render_student_list(request,students):
-#    return render(
-#        request,
-#        "StudentView/StudentViewIndex.html",
-#        {
-#            "students": students,
-#        },
-#    )
-
-#def add_manually(request):
-#    students = Student.objects.all().order_by("s_roll")
-#    return render_student_list(request, students)
-
-#def add_manually_class(request,className):
-#    students = Student.objects.all().order_by("s_roll")
-#    return render_student_list(request, students)
-
-#def add_manually_year(request, year):
-#    students = Student.objects.filter(s_year=year)
-#    return render_student_list(request, students)
-
-#https://stackoverflow.com/questions/57989320/django-filter-foreignkey-related-model-filtering
-#def add_manually_filtered(request, branch, section, year):
-#    students = Student.objects.filter(s_branch__branch__iexact=branch).filter(s_section__section__iexact=section).filter(s_year__year=year)
-#    return render_student_list(request, students)
-
-#def add_manually_filtered_2(request, branch, section):
-#    students = Student.objects.filter(s_branch__branch__iexact=branch).filter(s_section__section__iexact=section)
-#    return render_student_list(request, students)
-
-#def add_manually_filtered_3(request, branch):
-#    students = Student.objects.filter(s_branch__branch__iexact=branch)
-#    return render_student_list(request, students)
