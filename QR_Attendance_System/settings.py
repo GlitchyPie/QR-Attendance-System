5afd79468f1c7a1ee115783f156842041f7c1fff
GlitchyPie <71139548+GlitchyPie@users.noreply.github.com>
2025-09-14 17:42:44 +0100

    oops, wrong import for pytz

--- QR_Attendance_System/settings.py
+++ QR_Attendance_System/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-from pytz import pytz
+import pytz
 from datetime import datetime
 from pathlib import Path
 
