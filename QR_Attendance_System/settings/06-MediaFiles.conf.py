ae57688cf6999d2282a2a00a5d5e93d1896365c5
GlitchyPie <71139548+GlitchyPie@users.noreply.github.com>
2025-09-17 11:26:28 +0100

    Delete QR_Attendance_System/settings/06-MediaFiles.conf.py

--- QR_Attendance_System/settings/06-MediaFiles.conf.py
+++
@@ -1,8 +0,0 @@
-# Media files
-# https://docs.djangoproject.com/en/5.2/ref/settings/#std-setting-MEDIA_ROOT
-
-if DEBUG:
-    MEDIA_ROOT = f"{BASE_DIR}/media"
-else:
-    MEDIA_ROOT = ""
-    MEDIA_URL = ""
